91ddad470c12b2675e3032cfc107e590133b94c0
naour boutaina <naour_boutaina@emsi-edu.ma>
2022-11-15 09:18:50 +0100

    first commit

---
+++ Artisanaux.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Artisanaux.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Artisanaux.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext:DbContext
+    { //ctor double tabulation
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
